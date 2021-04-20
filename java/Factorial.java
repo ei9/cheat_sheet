23a30070b0e36ff984541029d817750f2617e799
ei9 <ei498@protonmail.com>
2021-04-20 21:23:59 +0800

    Add a method to count n!.

--- java/Factorial.java
+++ java/Factorial.java
@@ -8,10 +8,25 @@ public class Factorial {
         Scanner sc = new Scanner(System.in);
         System.out.print("N = ");
         int n = sc.nextInt();
+        sc.close();
+        System.out.println("N! = ");
+        System.out.println("For loop: " + usingForLoop(n));
+        System.out.println("Factorial: " + fac(n));
+    }
 
+    /*
+     * 使用for迴圈計算N!
+     */
+    static int usingForLoop(int n) {
         for (int i = n - 1 ; i > 0 ; i--)
             n *= i;
+        return n;
+    }
 
-        System.out.println("N! = " + n);
+    /*
+     * 使用遞迴計算N!
+     */
+    static int fac(int n) {
+        return n == 1 ? n : n * fac(n - 1);
     }
 }
